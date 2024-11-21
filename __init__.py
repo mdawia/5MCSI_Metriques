5b9ebf5cf8bfcceb41a9938b51485e2483f450df
mdawia <mdawia@outlook.fr>
2024-11-21 16:45:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+import requests
+import matplotlib.pyplot as plt
+import io
+import base64
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
+
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
@@ -12,7 +17,44 @@ def hello_world():
     return render_template('hello.html') #Commentaire
 
 
+@app.route('/commits/')
+def commits():
+    # Récupérer les commits via l'API GitHub
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = requests.get(url)
+    commits_data = response.json()
 
+    # Dictionnaire pour compter les commits par minute
+    commit_minutes = {}
+
+    for commit in commits_data:
+        commit_date = commit['commit']['author']['date']
+        commit_time = commit_date.split('T')[1]  # Extraire l'heure (HH:MM:SSZ)
+        commit_minute = commit_time[:5]  # Prendre les 5 premiers caractères (HH:MM)
+
+        if commit_minute in commit_minutes:
+            commit_minutes[commit_minute] += 1
+        else:
+            commit_minutes[commit_minute] = 1
+
+    # Créer le graphique
+    minutes = list(commit_minutes.keys())
+    commit_counts = list(commit_minutes.values())
+
+    fig, ax = plt.subplots()
+    ax.bar(minutes, commit_counts)
+
+    ax.set_xlabel('Minute')
+    ax.set_ylabel('Nombre de Commits')
+    ax.set_title('Commits par Minute')
+
+    # Sauvegarder le graphique dans un objet image et le convertir en base64
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    img_base64 = base64.b64encode(img.getvalue()).decode('utf8')
+
+    return render_template('commits.html', img_base64=img_base64)
 
 
 from flask import Flask, render_template_string, render_template, jsonify
