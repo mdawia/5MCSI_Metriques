f1a1959f8eafb0aafe62732f958ed9c28410e65e
mdawia <mdawia@outlook.fr>
2024-11-21 15:56:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,6 +15,39 @@ def hello_world():
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import json
+from datetime import datetime
+from urllib.request import urlopen
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')  # Commentaire
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
+
+# Nouvelle route pour /tawarano/
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')  # Timestamp brut
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin à °C
+        results.append({
+            'Jour': datetime.utcfromtimestamp(dt_value).strftime('%Y-%m-%d %H:%M:%S'),  # Conversion Timestamp
+            'temp': round(temp_day_value, 2)  # Arrondi à 2 décimales
+        })
+    return jsonify(results=results)
+
   
 if __name__ == "__main__":
   app.run(debug=True)
